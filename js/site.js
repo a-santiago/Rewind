5f0c3aeff440a0fefb9ff7f6073baf4b3504f58c
a-santiago <santiago.62596@gmail.com>
2021-07-14 10:30:25 -0400

    appComplete1.0.3
    Taco Cat app has been completed with all the lines displaying and are being used for all of the code to display

--- js/site.js
+++ js/site.js
@@ -1,1 +1,43 @@
-//I will write site specific JS in this file
+//step 1 get the word
+//Step 2 revese the word
+// we can use the same code to access letters in a string
+//array[index] - give me the value of array at the position index
+//string[index] - give me the letter in the string at the position index
+
+
+function getWord() {
+    let word = document.getElementById("tacoCat").value;
+
+    let revWord = flipWord(word);
+
+    displayResults(word, revWord);
+
+    let trueFalsePalindrome = document.getElementById("output2");
+    if (word === revWord) {
+        trueFalsePalindrome.innerHTML = "It's a palindrome!";
+    } else {
+        trueFalsePalindrome.innerHTML = "It's not a palindrome!";
+    }
+
+};
+
+function flipWord(word) {
+
+    let revWord = "";
+
+    for (let index = word.length - 1; index >= 0; index--) {
+        let letter = word[index];
+
+        revWord += letter;
+    }
+
+    return revWord;
+
+}
+
+function displayResults(word, revWord) {
+    let output = document.getElementById("output");
+
+    output.innerText = `You entered the word ${word}. Reversed is ${revWord}`;
+
+}
