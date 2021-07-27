fa0efd4995df48c3834a400511910372f1b59390
a-santiago <santiago.62596@gmail.com>
2021-07-26 22:24:12 -0400

    TacoCat Completed 1.5.0

--- js/site.js
+++ js/site.js
@@ -1,58 +1,23 @@
-//Step 1 - Get the word
-//Step 2 - Reverse the word
-//Strings are arrays of characters
-//We can use the same code to access the letters in a string
-//array[index] - give me the value of array at position index
-//string[index] - give me the letter in the string at position index
-
-//Frank
-//["F", "r", "a", "n", "k"]
-
-
-//wrapper function to control the app
-function getWord() {
+function reverseString() {
     //get the user's word
-    let word = document.getElementById("word").value;
 
-    //store the reversed word in a variable
-    let revWord = flipWord(word);
+    let userWord = document.getElementById("tacoCat").value;
+    let regexWord = userWord.replace(/[\W_]/g).toLowerCase();
+    let start = regexWord.length - 1;
+    let reverseWord = "";
 
-    //pass the original and reversed words to the display function
-    displayResults(word, revWord);
-};
-
-//this function does the "work" of the app, reversing a string
-function flipWord(word) {
-
-    //create a variable that will store the reversed word
-    let revWord = "";
-
-    //loop through the given word one letter at a time starting at the end
-    for (let index = word.length - 1; index >= 0; index--) {
-        //store the letter from the loop
-        let letter = word[index];
-
-        //add that letter to the variable created above
-        revWord += letter;
+    //for loop
+    for (let i = start; i >= 0; i--) {
+        reverseWord += regexWord[i];
     }
+    let output = document.getElementById("tacoOutput");
+    output.innerText = reverseWord;
 
-    //after the loop send back the reversed word
-    return revWord;
+    //palindrome
+    let trueFalsePalindrome = document.getElementById("palindrome");
+    if (regexWord === reverseWord) {
+        trueFalsePalindrome.innerText = "It's a palindrome";
+    } else {
+        trueFalsePalindrome.innerText = "It's not a palindrome";
+    }
 }
-
-
-//this function takes in information and prints it to the screen
-function displayResults(word, revWord) {
-    //access the output area
-    let output = document.getElementById("output");
-
-    //replace what is currently inside the <p> element with the phrase below
-    output.innerText = `You entered the word ${word}. Reversed is ${revWord}`;
-
-};
-
-//Notes for TacoCat/Palindrome
-//Basic completion is reversing the word and comparing it to the original - print if word equals reverse word yes or no
-//Better completion is handling capital vs lower case letters T != t
-//Extra credit is handling spaces and special characters
-//You can include the array function version of this exercise only if you have completed the for loop version
